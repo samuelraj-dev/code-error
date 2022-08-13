3c4cc24b60d2b43fcb3c392210b01ec80b259155
samuelraj-dev <samuelraj.dev@outlook.com>
2022-08-13 21:52:44 +0530

    david update 0.2

--- source/features/CreateFile.py
+++ source/features/CreateFile.py
@@ -1,11 +1,22 @@
 from source.filex.Talk import Talk
+from source.filex.GetCmd import GetCmd
 
 def CreateFile(audio_data):
     audio_data = audio_data.replace('create', '')
-    print(f'>> dumping into file.txt: {audio_data}')
+    audio_data = audio_data.strip()
+    print(f'>> Creating \'{audio_data}.txt\'...')
+    Talk(f'creating {audio_data}.txt')
 
-    with open(f'C:\\david\\file.txt', 'w') as file:
+    with open(f'C:\\david\\{audio_data}.txt', 'w') as file:
         audio_data = audio_data.strip()
-        file.write(audio_data)
-        print('>> successfully created the file...\n')
-        Talk('>> successfully created the file...')
+        print(f'>> Successfully created \'{audio_data}.txt\'...\n')
+        Talk(f'successfully created {audio_data}.txt...')
+
+    Talk(f'Please say the content for {audio_data}.txt...')
+    file_content = GetCmd(f'Content for {audio_data}.txt')
+
+    with open(f'C:\\david\\{audio_data}.txt', 'a') as file_append:
+        file_content = file_content.strip()
+        file_append.write(file_content)
+        print(f'>> Your content was successfully dumped into \'{audio_data}.txt\'...\n')
+        Talk(f'your content was successfully dumped into {audio_data}.txt...')
